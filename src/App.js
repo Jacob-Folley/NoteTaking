94a09102a1d609d79b7179adf95d6593a6360694
Jacob-Folley <jakebfolley@gmail.com>
2022-06-22 18:51:09 -0500

    Setting up my React Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import ApplicationViews from './ApplicationViews.js';
+// import { Route, useNavigate } from "react-router-dom";
+// import { Login } from "./Components/Auth/login";
+// import { Register } from "./Components/Auth/register";
+// import { HomeView } from "./Components/HomeView";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   const Navigate = useNavigate()
+//   return (
+//     <>
+//         {/* <Route
+            
+//             render={() => {
+//                 if (localStorage.getItem("lu_token")) {
+//                     return (
+//                         <>
+//                             <ApplicationViews />
+//                         </>
+//                     );
+//                 } else {
+//                     return <Navigate replace to="/login" />;
+//                 }
+//             }}
+//             />
 
-export default App;
+//         <Route path="/login">
+//             <Login />
+//         </Route>
+
+//         <Route path="/register">
+//             <Register />
+//         </Route> */}
+
+//         <Route>
+//           <HomeView />
+//         </Route>
+    
+//     </>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import ApplicationViews from './ApplicationViews.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApplicationViews />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
