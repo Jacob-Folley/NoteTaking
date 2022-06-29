0b576d18fa36316e685b1ce37b7b3cade9716869
Jacob-Folley <jakebfolley@gmail.com>
2022-06-28 19:09:10 -0500

    Setting up Functions and State for creating notes.

--- src/Components/Layout/notesView.js
+++ src/Components/Layout/notesView.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Content } from "./content";
 import { NewNote } from "./NewNote";
 import { SideBar } from "./sidebar";
@@ -12,21 +13,93 @@ import {
 
 export const NotesView = () => {
   const user = parseInt(localStorage.getItem("userId"));
+  const history = useHistory();
   const [newnote, setBoolean] = useState(false);
   const [notes, setNotes] = useState([]);
+  const [usernotes, setUserNotes] = useState([]);
   const [addnote, setNote] = useState({
     title: "",
     body: "",
-    tags: [],
-    datetime: Date.now(),
+    tags: "",
+    datetime: Date(),
     user_id: user,
   });
 
   useEffect(() => {
+    retrieveNotes();
+  }, []);
+
+  useEffect(() => {
+    setUserNotes(
+      notes.filter((note) => {
+        return note.user_id.id == user;
+      })
+    );
+  }, [notes]);
+
+  const changeFormState = (domEvent) => {
+    const copy = { ...addnote };
+    copy[domEvent.target.name] = domEvent.target.value;
+    setNote(copy);
+  };
+
+  const retrieveNotes = () => {
     getNotes().then((data) => {
       setNotes(data);
     });
-  }, []);
+  };
+
+  const newNote = () => {
+    return (
+      <>
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={changeFormState}
+        ></input>
+        <input placeholder="Tags"></input>
+        <p>{Date.now()}</p>
+        <textarea
+          name="body"
+          placeholder="NewNote"
+          onChange={changeFormState}
+        ></textarea>
+        <button
+          onClick={(evt) => {
+            evt.preventDefault();
+
+            const note = {
+              title: addnote.title,
+              body: addnote.body,
+              tags: addnote.tags,
+              datetime: addnote.datetime,
+              user_id: addnote.user_id,
+            };
+
+            createNote(note).then(() => history.push("/"));
+            retrieveNotes();
+            setBoolean(false);
+          }}
+        >
+          Save
+        </button>
+      </>
+    );
+  };
+
+  const Content = () => {
+    if (usernotes.length > 0) {
+      return (
+        <>
+          <h1>{usernotes[usernotes.length - 1].title}</h1>
+          <p>{usernotes[usernotes.length - 1].tags}</p>
+          <p>{usernotes[usernotes.length - 1].body}</p>
+        </>
+      );
+    } else {
+      return "";
+    }
+  };
 
   return (
     <>
@@ -35,6 +108,7 @@ export const NotesView = () => {
         <div className="sidebar">
           <SideBar />
         </div>
+
         {/* NotesView */}
         <div className="notesview">
           <button
@@ -51,30 +125,23 @@ export const NotesView = () => {
             <option>newest</option>
             <option>oldest</option>
           </select>
-        </div>
-        {/* Content */}
-        <div className="content">
-          {newnote ? <NewNote /> : <Content />}
-          {newnote ? (
-            <button
-              onClick={(evt) => {
-                setBoolean(false);
 
-                const note = {
-                  title: addnote.title,
-                  body: addnote.body,
-                  tags: addnote.tags,
-                  datetime: addnote.datetime,
-                  user_id: addnote.user_id,
-                };
-              }}
-            >
-              Save
-            </button>
-          ) : (
-            ""
-          )}
+          <div>
+            {usernotes.map((note) => {
+              return (
+                <>
+                  <div>
+                    <h1>{note.title}</h1>
+                    <p>{note.datetime}</p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
+
+        {/* Content */}
+        <div className="content">{newnote ? newNote() : Content()}</div>
       </div>
     </>
   );
