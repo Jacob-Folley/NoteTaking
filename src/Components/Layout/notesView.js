73c71f21688ba1997bdde420997d53be8a0fce71
Jacob-Folley <jakebfolley@gmail.com>
2022-06-29 19:43:40 -0500

    Able to click and view different notes through the notes view

--- src/Components/Layout/notesView.js
+++ src/Components/Layout/notesView.js
@@ -14,10 +14,12 @@ import {
 export const NotesView = () => {
   const user = parseInt(localStorage.getItem("userId"));
   const history = useHistory();
+  const [noteId, setNoteId] = useState(0);
   const [newnote, setBoolean] = useState(false);
   const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState({});
   const [usernotes, setUserNotes] = useState([]);
-  const [addnote, setNote] = useState({
+  const [addnote, setAddNote] = useState({
     title: "",
     body: "",
     tags: "",
@@ -30,6 +32,10 @@ export const NotesView = () => {
   }, []);
 
   useEffect(() => {
+    retrieveNote(noteId);
+  }, [noteId]);
+
+  useEffect(() => {
     setUserNotes(
       notes.filter((note) => {
         return note.user_id.id == user;
@@ -40,13 +46,31 @@ export const NotesView = () => {
   const changeFormState = (domEvent) => {
     const copy = { ...addnote };
     copy[domEvent.target.name] = domEvent.target.value;
-    setNote(copy);
+    setAddNote(copy);
   };
 
   const retrieveNotes = () => {
     getNotes().then((data) => {
       setNotes(data);
     });
+  };
+
+  const retrieveNote = (id) => {
+    getNote(id).then((data) => {
+      setNote(data);
+    });
+  };
+
+  const noteRetrieved = () => {
+    return (
+      <>
+        <h1>{note.title}</h1>
+        <p>{note.tags}</p>
+        <p>{note.body}</p>
+        <button>Edit</button>
+        <button>Delete</button>
+      </>
+    );
   };
 
   const newNote = () => {
@@ -87,6 +111,44 @@ export const NotesView = () => {
     );
   };
 
+  // const editNote = () => {
+  //   return (
+  //     <>
+  //       <input
+  //         name="title"
+  //         placeholder="Title"
+  //         onChange={changeFormState}
+  //       >{note.title}</input>
+  //       <input placeholder="Tags"></input>
+  //       <p>{Date.now()}</p>
+  //       <textarea
+  //         name="body"
+  //         placeholder="NewNote"
+  //         onChange={changeFormState}
+  //       >{note.body}</textarea>
+  //       <button
+  //         onClick={(evt) => {
+  //           evt.preventDefault();
+
+  //           const note = {
+  //             title: addnote.title,
+  //             body: addnote.body,
+  //             tags: addnote.tags,
+  //             datetime: addnote.datetime,
+  //             user_id: addnote.user_id,
+  //           };
+
+  //           updateNote(note).then(() => history.push("/"));
+  //           retrieveNotes();
+  //           setBoolean(false);
+  //         }}
+  //       >
+  //         Save
+  //       </button>
+  //     </>
+  //   );
+  // };
+
   const Content = () => {
     if (usernotes.length > 0) {
       return (
@@ -94,6 +156,8 @@ export const NotesView = () => {
           <h1>{usernotes[usernotes.length - 1].title}</h1>
           <p>{usernotes[usernotes.length - 1].tags}</p>
           <p>{usernotes[usernotes.length - 1].body}</p>
+          <button>Edit</button>
+          <button>Delete</button>
         </>
       );
     } else {
@@ -108,6 +172,7 @@ export const NotesView = () => {
         <div className="sidebar">
           <SideBar />
         </div>
+        {/* -------------------------------------------------------------------------------- */}
 
         {/* NotesView */}
         <div className="notesview">
@@ -131,7 +196,13 @@ export const NotesView = () => {
               return (
                 <>
                   <div>
-                    <h1>{note.title}</h1>
+                    <h1
+                      onClick={() => {
+                        setNoteId(note.id);
+                      }}
+                    >
+                      {note.title}
+                    </h1>
                     <p>{note.datetime}</p>
                   </div>
                 </>
@@ -139,9 +210,12 @@ export const NotesView = () => {
             })}
           </div>
         </div>
+        {/* -------------------------------------------------------------------------------- */}
 
         {/* Content */}
-        <div className="content">{newnote ? newNote() : Content()}</div>
+        <div className="content">
+          {newnote ? newNote() : noteId < 1 ? Content() : noteRetrieved()}
+        </div>
       </div>
     </>
   );
